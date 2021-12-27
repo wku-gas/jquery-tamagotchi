cdcf579df263adaf308f23d8e916f2b56572ef2a
William Ku <kucheech@gmail.com>
2021-12-27 14:00:53 +0800

    init tamagotchi with three buttons: eat, run and sleep

--- app.js
+++ app.js
@@ -1,3 +1,71 @@
+const moods = ['general1', 'general2', 'happy', 'sad', 'angry', 'back-turned', 'eyes-closed']
+
+const getRandomMood = () => moods[Math.floor(Math.random() * moods.length)]
+
+const setSprite = () => {
+  $('#tamagotchi').removeClass()
+  $('#tamagotchi').addClass(getRandomMood())
+}
+
+const setMood = (mood) => {
+  $('#tamagotchi').removeClass()
+  $('#tamagotchi').addClass(mood)
+}
+
+const eating = () => {
+  const eatingClass = $('#tamagotchi').attr('class')
+  $('#tamagotchi').removeClass()
+  $('#tamagotchi').addClass(eatingClass === 'eating1' ? 'eating2' : 'eating1')
+}
+
+const eatingAnimation = () => {
+  const eatingInterval = setInterval(eating, 200);
+  setTimeout(() => {
+    clearInterval(eatingInterval)
+    setMood(getRandomMood())
+  }, 2000)
+}
+
+const running = () => {
+  const runningClass = $('#tamagotchi').attr('class')
+  $('#tamagotchi').removeClass()
+  if (Math.random() < 0.05) {
+    $('#tamagotchi').addClass('running3')
+  } else {
+    $('#tamagotchi').addClass(runningClass === 'running1' ? 'running2' : 'running1')
+
+  }
+}
+
+const runningAnimation = () => {
+  const runningInterval = setInterval(running, 200);
+  setTimeout(() => {
+    clearInterval(runningInterval)
+    setMood(getRandomMood())
+  }, 5000)
+}
+
+const sleeping = () => {
+  const sleepingClass = $('#tamagotchi').attr('class')
+  $('#tamagotchi').removeClass()
+  $('#tamagotchi').addClass(sleepingClass === 'sleeping1' ? 'sleeping2' : 'sleeping1')
+}
+
+const sleepingAnimation = () => {
+  const sleepingInterval = setInterval(sleeping, 200);
+  setTimeout(() => {
+    clearInterval(sleepingInterval)
+    setMood(getRandomMood())
+  }, 3000)
+}
+
+
 $(() => {
-  console.log($("body"));
+  setMood('general1')
+  setInterval(() => {
+    setMood(getRandomMood())
+  }, 5000)
+  $('#eat').click(eatingAnimation)
+  $('#run').click(runningAnimation)
+  $('#sleep').click(sleepingAnimation)
 });
